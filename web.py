2ef60d113624a85c0edba8aee7b49a3c8f82924e
Mr maker <167637714+Mrmaker430@users.noreply.github.com>
2025-04-22 07:51:04 +0530

    Update web.py

--- web.py
+++ web.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot is running. Made with ♥️ by HxBots"
+    return "ʙᴏᴛ ɪꜱ ʀᴜɴɴɪɴɢ. Mᴀᴅᴇ ᴡɪᴛʜ ♥️ ʙʏ ༄ᶦᶰᵈ᭄࿐𝘗𝘢𝘳𝘢𝘥𝟎𝘹 么"
 
 def run():
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
