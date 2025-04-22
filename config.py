d745498232c6713207f10e98a766b1f56c6abb85
Mr maker <167637714+Mrmaker430@users.noreply.github.com>
2025-04-22 08:20:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,17 +4,17 @@ import os, re
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+OWNER = int(os.environ.get("OWNER", "1773034985"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "filerename4gbbot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "world_0f_parad0x")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002516091254"))
 
 DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
+DB_NAME = os.environ.get("DB_NAME", "Cluster0")
 
 STRING = os.environ.get("STRING", "")
-BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
+BOT_PIC = os.environ.get("BOT_PIC", "https://iili.io/3EtiRtt.md.jpg")
 
 
 
